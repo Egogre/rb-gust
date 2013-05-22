e6df7777865fae6e23ddc065dbc8aaec67b1510a
jfeaver <nathan.feaver@gmail.com>
2013-05-22 17:39:49 -0600

    combining the basic specs

--- spec/script_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Gust do
-  it "should have objects" do
-    path = "/Users/nathan/gems/gust2/spec/workbooks/basic_objects.xls"
-    o = Gust.load(path)
-    o.should be_a(Hash)
-  end
-end
